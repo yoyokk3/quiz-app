64fba3cb99542c224690dd538e4bc0ba66788b37
yoyokk3 <katou0503you74@gmail.com>
2020-03-23 14:03:42 +0900

    フロント画面とクイズ作成機能（ジャンルのみ）

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ルートパス呼び出し
+  root to: 'posts#index'
+  resources :posts, only: [:new, :create]
 end
